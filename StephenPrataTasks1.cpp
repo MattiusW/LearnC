960c528e1edc7ac3a957ae7706a5ecf78bcab9c0
Mateusz Walczyk <mattiusw@gmail.com>
2023-06-11 14:38:07 +0200

    Task 2

--- StephenPrataTasks1.cpp
+++ StephenPrataTasks1.cpp
@@ -1,17 +1,28 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 double calToCm(double cal);
+double bmi(int feet, double funt);
 
 int main(void)
 {
     double cal;
     double cm;
+    int feet;
+    double funt;
+    long bmiAmount;
 
     cout << "Enter you'r height in cal: ";
     cin >> cal;
     cm = calToCm(cal); 
     cout << "You'r height is: " << cm << " cm." << endl; 
+
+    cout << "Enter you'r height in feet: ";
+    cin >> feet;
+    cout << "Enter You'r weight in funt: ";
+    cin >> funt;
+    bmiAmount = bmi(feet, funt); 
+    cout << "You'r bmi is: " << bmiAmount << endl; 
 
 }
 
@@ -21,3 +32,15 @@ double calToCm(double cal)
     double amount = cal * cm;
     return amount;
 }
+
+double bmi(int feet, double funt)
+{
+    const long m = 0.0254; 
+    int height = feet * 12;
+    long meter = long(height) * m;
+    long kilo = funt / 2.2;
+    long bmi = kilo / (long(height) * long(height));   
+    
+    return bmi;
+}
+
